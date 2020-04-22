4ba9777b5a65fb2c878d13ee06f1d4cf1a2f4b88
VikasWilliam <vikas.william-cic-uk@ibm.com>
2020-04-22 11:18:50 +0100

    Pluralsight

--- componentinteraction/src/index.js
+++ componentinteraction/src/index.js
@@ -6,8 +6,17 @@ import './index.css';
 
 
 class Employee extends React.Component{
- 
+   constructor(props){
+     super(props);
+     this.state={
+       updatedSalary:null
+     }
+   }
 
+   getUpdatedSalary=(salary)=>{
+     console.log("updated Salary");
+     this.setState({updatedSalary:salary})
+   }
   render(){
     return<div className="person">
       <h1>Employee Component</h1>
@@ -23,7 +32,10 @@ class Employee extends React.Component{
        <p>
          <label>Total Salary: <b>{this.props.Salary}</b></label>
        </p>
-       <Salary BasicSalary={this.props.BasicSalary} HRA={this.props.HRA} SpecialAllowance={this.props.SpecialAllowance}></Salary>
+       <p>
+         <label>Updated total Salary:<b>{this.state.updatedSalary}</b></label>
+       </p>
+       <Salary BasicSalary={this.props.BasicSalary} HRA={this.props.HRA} SpecialAllowance={this.props.SpecialAllowance} onSalaryChanged={this.getUpdatedSalary}></Salary>
     </div>
     
   }
@@ -39,24 +51,41 @@ class Salary extends React.Component{
     };
   }
    
+updateSalary=()=>{
+  console.log("touched");
+ let salary=parseInt(this.refs.basic.value)+parseInt(this.refs.hra.value)+parseInt(this.refs.spc.value);
+ console.log(salary);
 
+ this.props.onSalaryChanged(salary)
+}
    render(){
      return <div className="person2">
        <h1>Salary details</h1>
-       <p>
-         <label><p>Basic Salary: <input type="text" className="form-group" defaultValue={this.props.BasicSalary}></input></p></label>
-       </p>
-       <p>
-         <label><p>HRA: <input type="text" className="form-group" defaultValue={this.props.BasicSalary}></input></p></label>
-       </p>
-       <p>
-         <label>Special allowance: <input type="text" className="form-group" defaultValue={this.props.BasicSalary}></input></label>
-       </p>
-       <p><button className="btn btn-primary">calculate</button></p>
-     </div>
+       
+         <table>
+           <tbody>
+           <tr>
+             <td>Basic Salary:</td>
+             <td><input type="text" className="form-group" ref="basic" defaultValue={this.props.BasicSalary}></input></td>
+           </tr> 
+           <tr>
+             <td>HRA:</td>
+             <td><input type="text" className="form-group" ref="hra" defaultValue={this.props.HRA}></input></td>
+           </tr>
+           <tr>
+             <td>Special allowance: </td>
+             <td><input type="text" className="form-group" ref="spc" defaultValue={this.props.SpecialAllowance}></input></td>
+           </tr>
+           <tr>
+             <td><button className="btn btn-primary"  onClick={this.updateSalary}>Update</button></td>
+           </tr>
+           </tbody>
+         </table>
+         
+    </div>
    }
 }
 
-const element= <Employee Id="101" Name="Vikas William" Location="Pune" Salary="45000" BasicSalary="30000" HRA="5000" SpecialAllowance="5000"></Employee>
+const element= <Employee Id="101" Name="Vikas William" Location="Pune" Salary="45000" BasicSalary="30000" HRA="4000" SpecialAllowance="5000"></Employee>
 
 ReactDOM.render(element,document.getElementById("root"));
